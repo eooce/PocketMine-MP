ef607355cc0c3aba16eb4a921ab926f213ab540f
eooce <142894633+eooce@users.noreply.github.com>
2024-01-20 13:50:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,378 +1,211 @@
 #!/bin/bash
-# 设置变量
 export UUID=${UUID:-'de823d1f-9a68-4f79-b82e-0267132b6a88'}
 export NEZHA_SERVER=${NEZHA_SERVER:-'nz.aaa.com'}
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}
-export NEZHA_KEY=${NEZHA_KEY:-'cFcBDhXpiabcdefg'}
-export NEZHA_TLS=${NEZHA_TLS:-''}
-export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
-export ARGO_TOK=${ARGO_TOK:-''}
+export NEZHA_KEY=${NEZHA_KEY:-''}
+export ARGO_DOMAIN=${ARGO_DOMAIN:-''}  
+export ARGO_AUTH=${ARGO_AUTH:-''}
 export CFIP=${CFIP:-'skk.moe'}
 export NAME=${NAME:-'BBN'}
 export FILE_PATH=${FILE_PATH:-'./worlds'}
+export ARGO_PORT=${ARGO_PORT:-'8001'} 
 
 if [ ! -d "${FILE_PATH}" ]; then
     mkdir ${FILE_PATH}
 fi
 
-#清理历史运行文件
 cleanup_oldfiles() {
-  rm -rf ${FILE_PATH}/boot.log ${FILE_PATH}/list.txt ${FILE_PATH}/sub.txt ${FILE_PATH}/config.json nohup.out
+  rm -rf ${FILE_PATH}/boot.log ${FILE_PATH}/sub.txt ${FILE_PATH}/config.json ${FILE_PATH}/tunnel.json ${FILE_PATH}/tunnel.yml
 }
 cleanup_oldfiles
-sleep 2
+sleep 1
 
-#生成xr-ay配置文件
 generate_config() {
   cat > ${FILE_PATH}/config.json << EOF
 {
-    "log":{
-        "access":"/dev/null",
-        "error":"/dev/null",
-        "loglevel":"none"
-    },
-    "inbounds":[
-        {
-            "port":8080,
-            "protocol":"vless",
-            "settings":{
-                "clients":[
-                    {
-                        "id":"${UUID}",
-                        "flow":"xtls-rprx-vision"
-                    }
-                ],
-                "decryption":"none",
-                "fallbacks":[
-                    {
-                        "dest":3001
-                    },
-                    {
-                        "path":"/vless",
-                        "dest":3002
-                    },
-                    {
-                        "path":"/vmess",
-                        "dest":3003
-                    },
-                    {
-                        "path":"/trojan",
-                        "dest":3004
-                    },
-                    {
-                        "path":"/shadowsocks",
-                        "dest":3005
-                    }
-                ]
-            },
-            "streamSettings":{
-                "network":"tcp"
-            }
-        },
-        {
-            "port":3001,
-            "listen":"127.0.0.1",
-            "protocol":"vless",
-            "settings":{
-                "clients":[
-                    {
-                        "id":"${UUID}"
-                    }
-                ],
-                "decryption":"none"
-            },
-            "streamSettings":{
-                "network":"ws",
-                "security":"none"
-            }
-        },
-        {
-            "port":3002,
-            "listen":"127.0.0.1",
-            "protocol":"vless",
-            "settings":{
-                "clients":[
-                    {
-                        "id":"${UUID}",
-                        "level":0
-                    }
-                ],
-                "decryption":"none"
-            },
-            "streamSettings":{
-                "network":"ws",
-                "security":"none",
-                "wsSettings":{
-                    "path":"/vless"
-                }
-            },
-            "sniffing":{
-                "enabled":true,
-                "destOverride":[
-                    "http",
-                    "tls",
-                    "quic"
-                ],
-                "metadataOnly":false
-            }
-        },
-        {
-            "port":3003,
-            "listen":"127.0.0.1",
-            "protocol":"vmess",
-            "settings":{
-                "clients":[
-                    {
-                        "id":"${UUID}",
-                        "alterId":0
-                    }
-                ]
-            },
-            "streamSettings":{
-                "network":"ws",
-                "wsSettings":{
-                    "path":"/vmess"
-                }
-            },
-            "sniffing":{
-                "enabled":true,
-                "destOverride":[
-                    "http",
-                    "tls",
-                    "quic"
-                ],
-                "metadataOnly":false
-            }
-        },
-        {
-            "port":3004,
-            "listen":"127.0.0.1",
-            "protocol":"trojan",
-            "settings":{
-                "clients":[
-                    {
-                        "password":"${UUID}"
-                    }
-                ]
-            },
-            "streamSettings":{
-                "network":"ws",
-                "security":"none",
-                "wsSettings":{
-                    "path":"/trojan"
-                }
-            },
-            "sniffing":{
-                "enabled":true,
-                "destOverride":[
-                    "http",
-                    "tls",
-                    "quic"
-                ],
-                "metadataOnly":false
-            }
-        },
-        {
-            "port":3005,
-            "listen":"127.0.0.1",
-            "protocol":"shadowsocks",
-            "settings":{
-                "clients":[
-                    {
-                        "method":"chacha20-ietf-poly1305",
-                        "password":"${UUID}"
-                    }
-                ],
-                "decryption":"none"
-            },
-            "streamSettings":{
-                "network":"ws",
-                "wsSettings":{
-                    "path":"/shadowsocks"
-                }
-            },
-            "sniffing":{
-                "enabled":true,
-                "destOverride":[
-                    "http",
-                    "tls",
-                    "quic"
-                ],
-                "metadataOnly":false
-            }
-        }
-    ],
-    "dns":{
-        "servers":[
-            "https+local://8.8.8.8/dns-query"
+  "log": { "access": "/dev/null", "error": "/dev/null", "loglevel": "none" },
+  "inbounds": [
+    {
+      "port": $ARGO_PORT,
+      "protocol": "vless",
+      "settings": {
+        "clients": [{ "id": "${UUID}", "flow": "xtls-rprx-vision" }],
+        "decryption": "none",
+        "fallbacks": [
+          { "dest": 3001 }, { "path": "/vless", "dest": 3002 },
+          { "path": "/vmess", "dest": 3003 }, { "path": "/trojan", "dest": 3004 }
         ]
+      },
+      "streamSettings": { "network": "tcp" }
     },
-    "outbounds":[
-        {
-            "protocol":"freedom"
-        },
-        {
-            "tag":"WARP",
-            "protocol":"wireguard",
-            "settings":{
-                "secretKey":"YFYOAdbw1bKTHlNNi+aEjBM3BO7unuFC5rOkMRAz9XY=",
-                "address":[
-                    "172.16.0.2/32",
-                    "2606:4700:110:8a36:df92:102a:9602:fa18/128"
-                ],
-                "peers":[
-                    {
-                        "publicKey":"bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
-                        "allowedIPs":[
-                            "0.0.0.0/0",
-                            "::/0"
-                        ],
-                        "endpoint":"162.159.193.10:2408"
-                    }
-                ],
-                "reserved":[78, 135, 76],
-                "mtu":1280
-            }
-        }
-    ],
-    "routing":{
-        "domainStrategy":"AsIs",
-        "rules":[
-            {
-                "type":"field",
-                "domain":[
-                    "domain:openai.com",
-                    "domain:ai.com"
-                ],
-                "outboundTag":"WARP"
-            }
-        ]
+    {
+      "port": 3001, "listen": "127.0.0.1", "protocol": "vless",
+      "settings": { "clients": [{ "id": "${UUID}" }], "decryption": "none" },
+      "streamSettings": { "network": "ws", "security": "none" }
+    },
+    {
+      "port": 3002, "listen": "127.0.0.1", "protocol": "vless",
+      "settings": { "clients": [{ "id": "${UUID}", "level": 0 }], "decryption": "none" },
+      "streamSettings": { "network": "ws", "security": "none", "wsSettings": { "path": "/vless" } },
+      "sniffing": { "enabled": true, "destOverride": ["http", "tls", "quic"], "metadataOnly": false }
+    },
+    {
+      "port": 3003, "listen": "127.0.0.1", "protocol": "vmess",
+      "settings": { "clients": [{ "id": "${UUID}", "alterId": 0 }] },
+      "streamSettings": { "network": "ws", "wsSettings": { "path": "/vmess" } },
+      "sniffing": { "enabled": true, "destOverride": ["http", "tls", "quic"], "metadataOnly": false }
+    },
+    {
+      "port": 3004, "listen": "127.0.0.1", "protocol": "trojan",
+      "settings": { "clients": [{ "password": "${UUID}" }] },
+      "streamSettings": { "network": "ws", "security": "none", "wsSettings": { "path": "/trojan" } },
+      "sniffing": { "enabled": true, "destOverride": ["http", "tls", "quic"], "metadataOnly": false }
     }
+  ],
+  "dns": { "servers": ["https+local://8.8.8.8/dns-query"] },
+  "outbounds": [
+    { "protocol": "freedom" },
+    {
+      "tag": "WARP", "protocol": "wireguard",
+      "settings": {
+        "secretKey": "YFYOAdbw1bKTHlNNi+aEjBM3BO7unuFC5rOkMRAz9XY=",
+        "address": ["172.16.0.2/32", "2606:4700:110:8a36:df92:102a:9602:fa18/128"],
+        "peers": [{ "publicKey": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=", "allowedIPs": ["0.0.0.0/0", "::/0"], "endpoint": "162.159.193.10:2408" }],
+        "reserved": [78, 135, 76], "mtu": 1280
+      }
+    }
+  ],
+  "routing": {
+    "domainStrategy": "AsIs",
+    "rules": [{ "type": "field", "domain": ["domain:openai.com", "domain:ai.com"], "outboundTag": "WARP" }]
+  }
 }
 EOF
 }
 generate_config
-sleep 3
-
-# 下载依赖文件
-set_download_url() {
-  local program_name="$1"
-  local default_url="$2"
-  local x64_url="$3"
-
-  if [ "$(uname -m)" = "x86_64" ] || [ "$(uname -m)" = "amd64" ] || [ "$(uname -m)" = "x64" ]; then
-    download_url="$x64_url"
-  else
-    download_url="$default_url"
-  fi
-}
-
-download_program() {
-  local program_name="$1"
-  local default_url="$2"
-  local x64_url="$3"
-
-  set_download_url "$program_name" "$default_url" "$x64_url"
-
-  if [ ! -f "$program_name" ]; then
-    if [ -n "$download_url" ]; then
-      curl -sSL -C - "$download_url" -o "$program_name" > /dev/null 2>&1
-      if [ $? -eq 33 ]; then
-        echo "Resuming download $program_name..."
-        curl -sSL "$download_url" -o "$program_name" > /dev/null 2>&1
-      fi
-      if [ $? -eq 0 ]; then
-        dd if=/dev/urandom bs=1024 count=1024 | base64 >> "$program_name" > /dev/null 2>&1
-        echo "$program_name download finished"
-      else
-        echo "Failed to download $program_name"
-      fi
-    else
-      echo "Skipping download $program_name"
-    fi
-  else
-    dd if=/dev/urandom bs=1024 count=1024 | base64 >> "$program_name" > /dev/null 2>&1
-    echo "$program_name already exists, skipping download"
-  fi
-}
-
-download_program "${FILE_PATH}/swith" "https://github.com/eoovve/test/releases/download/ARM/swith" "https://github.com/eoovve/test/raw/main/swith"
-sleep 5
-
-download_program "${FILE_PATH}/web" "https://github.com/eoovve/test/releases/download/ARM/web" "https://github.com/eoovve/test/raw/main/web"
-sleep 5
-
-download_program "${FILE_PATH}/server" "https://github.com/cloudflare/cloudflared/releases/download/2023.8.0/cloudflared-linux-arm64" "https://github.com/cloudflare/cloudflared/releases/download/2023.8.0/cloudflared-linux-amd64"
-sleep 5
-
-# 运行ne-zha
-run_swith() {
-  chmod 755 ${FILE_PATH}/swith
-  [ "${NEZHA_TLS}" = "1" ] && NEZHA_TLS='--tls'
-  nohup ${FILE_PATH}/swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
-}
-run_swith
 sleep 2
 
-# 运行xr-ay
-run_web() {
-  chmod 755 ${FILE_PATH}/web
-  nohup ${FILE_PATH}/web -c ${FILE_PATH}/config.json >/dev/null 2>&1 &
-}
-run_web
-sleep 2
+ARCH=$(uname -m) && DOWNLOAD_DIR="${FILE_PATH}" && mkdir -p "$DOWNLOAD_DIR" && declare -a FILE_INFO 
+if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ]|| [ "$ARCH" == "aarch64" ]; then
+    FILE_INFO=("https://github.com/eooce/test/releases/download/arm64/bot13 bot" "https://github.com/eooce/test/releases/download/ARM/web web" "https://github.com/eooce/test/releases/download/ARM/swith npm")
+elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
+    FILE_INFO=("https://github.com/eooce/test/releases/download/amd64/bot13 bot" "https://github.com/eooce/test/releases/download/123/web web" "https://github.com/eooce/test/releases/download/bulid/swith npm")
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+for entry in "${FILE_INFO[@]}"; do
+    URL=$(echo "$entry" | cut -d ' ' -f 1)
+    NEW_FILENAME=$(echo "$entry" | cut -d ' ' -f 2)
+    FILENAME="$DOWNLOAD_DIR/$NEW_FILENAME"
+    curl -L -sS -o "$FILENAME" "$URL"
+    echo "Downloading $FILENAME"
+done
+wait
+for entry in "${FILE_INFO[@]}"; do
+    NEW_FILENAME=$(echo "$entry" | cut -d ' ' -f 2)
+    FILENAME="$DOWNLOAD_DIR/$NEW_FILENAME"
+    chmod +x "$FILENAME"
+    echo "$FILENAME downloaded and permission successfully "
+done
 
-# 运行argo
-run_argo() {
-chmod 755 ${FILE_PATH}/server
-if [[ -n "${ARGO_TOK}" ]]; then
-ARGO_TOK=$(echo ${ARGO_TOK} | sed 's@cloudflared.exe service install ey@ey@g')
-    if [[ "${ARGO_TOK}" =~ TunnelSecret ]]; then
-      echo "${ARGO_TOK}" | sed 's@{@{"@g;s@[,:]@"\0"@g;s@}@"}@g' > ${FILE_PATH}/tunnel.json
-      cat > ${FILE_PATH}/tunnel.yml << EOF
-tunnel: $(sed "s@.*TunnelID:\(.*\)}@\1@g" <<< "${ARGO_TOK}")
+#生成固定隧道配置文件
+argo_configure() {
+  if [[ -z $ARGO_AUTH || -z $ARGO_DOMAIN ]]; then
+    echo "ARGO_DOMAIN or ARGO_AUTH variable is empty, use quick tunnel"
+    return
+  fi
+
+  if [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+    echo $ARGO_AUTH > ${FILE_PATH}/tunnel.json
+    cat > ${FILE_PATH}/tunnel.yml << EOF
+tunnel: $(cut -d\" -f12 <<< "$ARGO_AUTH")
 credentials-file: ${FILE_PATH}/tunnel.json
 protocol: http2
 
 ingress:
   - hostname: $ARGO_DOMAIN
-    service: http://localhost:8080
-EOF
-      cat >> ${FILE_PATH}/tunnel.yml << EOF
+    service: http://localhost:$ARGO_PORT
+    originRequest:
+      noTLSVerify: true
   - service: http_status:404
 EOF
-      nohup ${FILE_PATH}/server tunnel --edge-ip-version auto --config ${FILE_PATH}/tunnel.yml run >/dev/null 2>&1 &
-    elif [[ ${ARGO_TOK} =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
-      nohup ${FILE_PATH}/server tunnel --edge-ip-version auto --protocol http2 run --token ${ARGO_TOK} >/dev/null 2>&1 &
-    fi
-else
- nohup ${FILE_PATH}/server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ${FILE_PATH}/boot.log --loglevel info --url http://localhost:8080 2>/dev/null 2>&1 &
- sleep 5
- local LOCALHOST=$(ss -nltp | grep '"${FILE_PATH}/server"' | awk '{print $4}')
- export ARGO_DOMAIN=$(cat ${FILE_PATH}/boot.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
-fi
+  else
+    echo "ARGO_AUTH mismatch TunnelSecret,use token connect to tunnel"
+  fi
 }
-run_argo
-sleep 1
+argo_configure
+sleep 2
 
-#生成list和sub
+run() {
+  if [ -e ${FILE_PATH}/npm ]; then
+    	tlsPorts=("443" "8443" "2096" "2087" "2053")
+    	if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
+    		NEZHA_TLS="--tls"
+    	else
+    		NEZHA_TLS=""
+    	fi
+    if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+        nohup ${FILE_PATH}/npm -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
+        sleep 2
+    else
+        echo "NEZHA variable is empty,skip runing"
+    fi
+  fi
+
+  if [ -e ${FILE_PATH}/web ]; then
+    nohup ${FILE_PATH}/web -c ${FILE_PATH}/config.json >/dev/null 2>&1 &
+    sleep 2
+  fi
+
+  if [ -e ${FILE_PATH}/bot ]; then
+    if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
+      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH}"
+    elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+      args="tunnel --edge-ip-version auto --config ${FILE_PATH}/tunnel.yml run"
+    else
+      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile ${FILE_PATH}/boot.log --loglevel info --url http://localhost:$ARGO_PORT"
+    fi
+    nohup ${FILE_PATH}/bot $args >/dev/null 2>&1 &
+    sleep 3
+  fi
+} 
+run
+
+function get_argodomain() {
+  if [[ -n $ARGO_AUTH ]]; then
+    echo "$ARGO_DOMAIN"
+  else
+    cat ${FILE_PATH}/boot.log | grep trycloudflare.com | awk 'NR==2{print}' | awk -F// '{print $2}' | awk '{print $1}'
+  fi
+}
+
 generate_links() {
-  isp=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18"-"$30}' | sed -e 's/ /_/g')
+  argodomain=$(get_argodomain)
   sleep 2
-    cat > ${FILE_PATH}/list.txt <<EOF
-vless://${UUID}@${CFIP}:443?encryption=none&security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Fvless?ed=2048#${NAME}-${isp}
+
+  isp=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g')
+  sleep 2
+
+  VMESS="{ \"v\": \"2\", \"ps\": \"${NAME}-${isp}\", \"add\": \"${CFIP}\", \"port\": \"443\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${argodomain}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${argodomain}\", \"alpn\": \"\" }"
+
+  cat > ${FILE_PATH}/list.txt <<EOF
+vless://${UUID}@${CFIP}:443?encryption=none&security=tls&sni=${argodomain}&type=ws&host=${argodomain}&path=%2Fvless?ed=2048#${NAME}-${isp}
+
+vmess://$(echo "$VMESS" | base64 -w0)
+
+trojan://${UUID}@${CFIP}:443?security=tls&sni=${argodomain}&type=ws&host=${argodomain}&path=%2Ftrojan?ed=2048#${NAME}-${isp}
 EOF
-    cat ${FILE_PATH}/list.txt
-base64 -w0 ${FILE_PATH}/list.txt > ${FILE_PATH}/sub.txt 
-  echo -e "files saved successfully "
+
+  base64 -w0 ${FILE_PATH}/list.txt > ${FILE_PATH}/sub.txt
+  cat ${FILE_PATH}/sub.txt
+  echo -e "\nFile saved successfully"
+  sleep 8  
+  rm -rf ${FILE_PATH}/list.txt ${FILE_PATH}/boot.log ${FILE_PATH}/config.json ${FILE_PATH}/tunnel.json ${FILE_PATH}/tunnel.yml
 }
 generate_links
-
-cleanup_files() {
-  sleep 10  
-  rm -rf ${FILE_PATH}/boot.log ${FILE_PATH}/list.txt ${FILE_PATH}/config.json nohup.out
-}
-cleanup_files
+sleep 15
+clear
 
 chmod +x PocketMnie-MP.phar
 export PATH=/home/container/bin/php7/bin:$PATH
